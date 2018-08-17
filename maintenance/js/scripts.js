3589145f9332d7fddf68d50b488378ffd06c9008
colorsailing <dev.zakariaelfakhry@gmail.com>
2018-08-17 19:00:58 +0100

    add subscriber and contact form emailing, correct typo and change background phone numbers

--- maintenance/js/scripts.js
+++ maintenance/js/scripts.js
@@ -189,7 +189,7 @@ jQuery(window).on('load', function(){
         var name = $("#name").val();
         var email = $("#email_address").val();
         var message = $("#message").val();
-        var dataString = 'name=' + name + '&email=' + email + '&message=' + message;
+        var dataString = 'name=' + name + '&email=' + email + '&message=' + message+ '&siteStatus=maintenance';
 
         function isValidEmail(emailAddress) {
             var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
@@ -197,18 +197,16 @@ jQuery(window).on('load', function(){
         };
 
         if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
-          Email.send(email,
-              "contact@colorsailing.com",
-              "Message from "+name,
-              message,
-              {
-                token: "c13ccb77-6d0b-4aab-ac29-4fde7fb5dabe",
-                callback: function() {
-                          $('#contact-form .success').fadeIn(1000);
-                          $('#contact-form .error').fadeOut(500);
-                          $("#contact-form")[0].reset();
-                      }
-              });
+          $.ajax({
+            type: "POST",
+            url: "https://script.google.com/macros/s/AKfycbwy58FFj7vV9pILbHpLXfInbP1QA2znqIqnc0bGjVazJ8JzK_o/exec",
+            data: dataString,
+            success: function() {
+                      $('#contact-form .success').fadeIn(1000);
+                      $('#contact-form .error').fadeOut(500);
+                      $("#contact-form")[0].reset();
+                  }
+          });
         } else {
             $('.error').fadeIn(1000);
             $('.success').fadeOut(500);
@@ -216,6 +214,40 @@ jQuery(window).on('load', function(){
 
         return false;
     });
+
+  /* =================================
+  ===  Subscribe FORM  - Menu       ====
+  =================================== */
+  $("#subscribe-form").on('submit', function(e) {
+    e.preventDefault();
+    var email = $("#subscribe_email").val();
+    var dataString='email='+email+ '&siteStatus=maintenance';
+
+    function isValidEmail(emailAddress) {
+      var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+      return pattern.test(emailAddress);
+    };
+
+    if (isValidEmail(email)) {
+
+      $('#subscribe-form .error').fadeOut(500);
+      $.ajax({
+        type: "POST",
+        url: "https://script.google.com/a/colorsailing.com/macros/s/AKfycbzXYVUMdaqmosXfGum8afrFfOdwUs4qZJGXonXu/exec",
+        data: dataString,
+        success: function() {
+              $('#subscribe-form .success').fadeIn(1000);
+              $('#subscribe-form .error').fadeOut(500);
+              $("#subscribe-form")[0].reset();
+            }
+          });
+    } else {
+      $('.error').fadeIn(1000);
+      $('.success').fadeOut(500);
+    }
+
+    return false;
+  });
 
     /* =================================
     ===  CONTACT FORM  - Popup      ====
